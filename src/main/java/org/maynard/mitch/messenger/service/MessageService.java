b1d6088668707b5134e38e6e2aacb308f1678619
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2015-05-30 18:31:17 -0500

    Lesson: Building Service Stubs (mock database)

--- src/main/java/org/maynard/mitch/messenger/service/MessageService.java
+++ src/main/java/org/maynard/mitch/messenger/service/MessageService.java
@@ -1,19 +1,54 @@
 package org.maynard.mitch.messenger.service;
 
+import org.maynard.mitch.messenger.database.DatabaseClass;
 import org.maynard.mitch.messenger.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService
 {
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
+
+    public MessageService()
+    {
+        messages.put( 1L, new Message( 1, "Message 1", "Mitch" ) );
+        messages.put( 2L, new Message( 2, "Message 2", "Mitch" ) );
+    }
+
     public List<Message> getAllMessages()
     {
-        Message message1 = new Message( 1, "Hello World1", "Mitch" );
-        Message message2 = new Message( 2, "Hello World2", "Mitch" );
-        List<Message> messages = new ArrayList<>(  );
-        messages.add( message1 );
-        messages.add( message2 );
-        return messages;
+        return new ArrayList<>( messages.values() );
+    }
+
+    public Message getMessage( long id )
+    {
+        return messages.get( id );
+    }
+
+    public Message addMessage( Message message )
+    {
+        message.setId( messages.size() + 1 );
+        messages.put( message.getId(), message );
+        return message;
+    }
+
+    public Message updateMessage( Message message )
+    {
+        if ( message.getId() <= 0 )
+        {
+            message = null;
+        }
+        else
+        {
+            messages.put( message.getId(), message );
+        }
+        return message;
+    }
+
+    public Message removeMessage( long id )
+    {
+        return messages.remove( id );
     }
 }
