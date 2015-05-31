b38b63d8c06c326a2d3488d74cff40b614afb0bb
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2015-05-30 23:26:54 -0500

    Lesson: Accessing Path params

--- src/main/java/org/maynard/mitch/messenger/resources/MessageResource.java
+++ src/main/java/org/maynard/mitch/messenger/resources/MessageResource.java
@@ -5,19 +5,29 @@ import org.maynard.mitch.messenger.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 //todo is the slash necessary?
-@Path("/messages")
+@Path( "/messages" )
 public class MessageResource
 {
     MessageService messageService = new MessageService();
+
     @GET
-    @Produces( MediaType.APPLICATION_XML)
+    @Produces( MediaType.APPLICATION_XML )
     public List<Message> getMessages()
     {
         return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path( "/{messageId}" )
+    @Produces( MediaType.APPLICATION_XML)
+    public Message test(@PathParam( "messageId" )long messageId)
+    {
+        return messageService.getMessage( messageId );
     }
 }
