e50d0b285861bd1541a7e5136facd9949db63b1e
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2015-05-31 21:56:27 -0500

    Lesson: The Param Annotations

---
+++ src/main/java/org/maynard/mitch/messenger/resources/DemoResource.java
@@ -0,0 +1,23 @@
+package org.maynard.mitch.messenger.resources;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path( "/demo" )
+@Consumes( MediaType.TEXT_PLAIN )
+@Produces( MediaType.TEXT_PLAIN )
+public class DemoResource
+{
+    @GET
+    @Path( "annotations" )
+    public String getParamUsingAnnotations( @MatrixParam( "param" ) String matrixParam,
+                                            @HeaderParam( "authSessionId" ) String headerParam,
+                                            @CookieParam( "JSESSIONID" ) String cookie )
+    {
+        //Matrix parameters are seperated on the url by semicolons instead of the leading question mark and ampersands.
+        //Header parameters are custom values sent in on the request via the header. Can be used for sending authentication tokens.
+        //Cookie parameters are used for reading cookies that are set in the request
+        //There is also FormParam, which is used when submitting an HTML form. This is rarely used for REST APIs.
+        return "Matrix Param: " + matrixParam + ", Header Param: " + headerParam + ", Cookie Param: " + cookie;
+    }
+}
