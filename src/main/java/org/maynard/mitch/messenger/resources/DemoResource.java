e9f554c141ec98f199cd492c7315896f19c1d80e
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2015-05-31 22:09:27 -0500

    reformatted

--- src/main/java/org/maynard/mitch/messenger/resources/DemoResource.java
+++ src/main/java/org/maynard/mitch/messenger/resources/DemoResource.java
@@ -18,6 +18,6 @@ public class DemoResource
         //Header parameters are custom values sent in on the request via the header. Can be used for sending authentication tokens.
         //Cookie parameters are used for reading cookies that are set in the request
         //There is also FormParam, which is used when submitting an HTML form. This is rarely used for REST APIs.
-        return "Matrix Param: " + matrixParam + ", Header Param: " + headerParam + ", Cookie Param: " + cookie;
+        return "Matrix Param: " + matrixParam + "\nHeader Param: " + headerParam + "\nCookie Param: " + cookie;
     }
 }
