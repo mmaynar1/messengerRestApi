a6de785fca3a4b672c84c3ab9625006d2ea0b6d2
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2015-06-04 21:39:45 -0500

    Lesson: Sending Status Codes and Location Headers

--- src/main/java/org/maynard/mitch/messenger/resources/MessageResource.java
+++ src/main/java/org/maynard/mitch/messenger/resources/MessageResource.java
@@ -4,9 +4,20 @@ import org.maynard.mitch.messenger.model.Message;
 import org.maynard.mitch.messenger.resources.beans.MessageFilterBean;
 import org.maynard.mitch.messenger.service.MessageService;
 
-import javax.ws.rs.*;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 //todo is the slash necessary?
@@ -40,33 +51,50 @@ public class MessageResource
 
     @GET
     @Path("/{messageId}")
-    public Message getMessage( @PathParam( "messageId" ) long messageId )
+    public Message getMessage( @PathParam("messageId") long messageId )
     {
         return messageService.getMessage( messageId );
     }
 
     @POST
-    public Message addMessage( Message message )
+    public Response addMessage( Message message, @Context UriInfo uriInfo )
     {
-        return messageService.addMessage( message );
+        Message newMessage = messageService.addMessage( message );
+
+        /*This works but could be better
+        return Response.status( Response.Status.CREATED )
+                .entity( newMessage )
+                .build();*/
+
+       /* Add throws URISyntaxException and you get the same thing + url in response header
+            return Response.created( new URI("/webapi/messages/" + newMessage.getId()) )
+                .entity( newMessage )
+                .build();*/
+
+        /* Best way to do it */
+        String newId = String.valueOf( newMessage.getId() );
+        URI uri = uriInfo.getAbsolutePathBuilder().path( newId ).build();
+        return Response.created( uri )
+                .entity( newMessage )
+                .build();
     }
 
     @PUT
-    @Path("/{messageId}")
-    public Message updateMessage( @PathParam("messageId") long messageId, Message message )
+    @Path( "/{messageId}" )
+    public Message updateMessage( @PathParam( "messageId" ) long messageId, Message message )
     {
         message.setId( messageId );
         return messageService.updateMessage( message );
     }
 
     @DELETE
-    @Path("/{messageId}")
-    public void deleteMessage( @PathParam("messageId") long messageId )
+    @Path( "/{messageId}" )
+    public void deleteMessage( @PathParam( "messageId" ) long messageId )
     {
         messageService.removeMessage( messageId );
     }
 
-    @Path("/{messageId}/comments")
+    @Path( "/{messageId}/comments" )
     public CommentResource getCommentResource()
     {
         return new CommentResource();
